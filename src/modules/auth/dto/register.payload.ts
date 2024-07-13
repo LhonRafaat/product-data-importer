f616a49c83eca7c45d9533ebe4bcb2f2fa1e07f4
lhonRafaat <lhontaekwondo@gmail.com>
2024-07-13 18:47:54 +0300

    added  findall products

--- src/modules/auth/dto/register.payload.ts
+++ src/modules/auth/dto/register.payload.ts
@@ -24,5 +24,5 @@ export class RegisterPayload {
 
   @ApiProperty()
   @IsOptional()
-  isAdmin?: boolean;
+  isAdmin?: boolean; // added this to dto for testing purposes
 }
--- src/modules/products/products.controller.ts
+++ src/modules/products/products.controller.ts
@@ -6,14 +6,36 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
+  Req,
+  Query,
 } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
-import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import {
+  ApiBearerAuth,
+  ApiExtraModels,
+  ApiOkResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+import { TProduct } from './models/product.model';
+import {
+  Action,
+  IQuery,
+  IRequest,
+  TResponse,
+  getResponseType,
+} from '../../common/helper/common-types';
+import { QueryTypes } from '../../common/decorators/query.decorator';
+import { AccessTokenGuard } from '../../common/guards/jwt.guard';
+import { AbilitiesGuard } from '../../common/guards/abilities.guard';
+import { checkAbilities } from '../../common/decorators/abilities.decorator';
 
 @Controller('products')
 @ApiTags('Products')
+@ApiBearerAuth()
+@ApiExtraModels(TProduct) // this decorator helps us to resolve TProduct class when used in getResponseType function
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
@@ -30,8 +52,15 @@ export class ProductsController {
   }
 
   @Get()
-  async findAll() {
-    return await this.productsService.findAll();
+  @ApiOkResponse(getResponseType(TProduct))
+  @QueryTypes()
+  @UseGuards(AccessTokenGuard, AbilitiesGuard)
+  @checkAbilities({ action: Action.Read, subject: TProduct })
+  async findAll(
+    @Req() req: IRequest,
+    @Query() query: IQuery,
+  ): Promise<TResponse<TProduct>> {
+    return await this.productsService.findAll(req, query);
   }
 
   @Get(':id')
