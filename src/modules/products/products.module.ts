fc8116cd824e84dc92b56c3a4d606e3ca56e8e58
lhonRafaat <lhontaekwondo@gmail.com>
2024-07-13 17:21:12 +0300

    imported product schema in product module

--- src/modules/products/products.module.ts
+++ src/modules/products/products.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Product } from './schemas/product.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: 'Product',
+        schema: Product,
+      },
+    ]),
+  ],
   controllers: [ProductsController],
   providers: [ProductsService],
 })
