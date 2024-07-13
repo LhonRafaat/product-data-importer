8bce94aabc6e6ff051b7060a8486db26ce13a42a
lhonRafaat <lhontaekwondo@gmail.com>
2024-07-13 17:48:39 +0300

    updated product schema and model

--- src/modules/products/models/product.model.ts
+++ src/modules/products/models/product.model.ts
@@ -1,61 +1,27 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { ObjectId } from 'mongoose';
 
 export class TProduct {
+  @ApiProperty()
   _id: string;
+
+  @ApiProperty()
   name: string;
-  type: string;
-  shortDescription: string;
+
+  @ApiProperty()
   description: string;
-  vendorId: string;
-  manufacturerId: string;
+
+  @ApiProperty()
+  vendorId: ObjectId;
+
+  @ApiProperty()
   storefrontPriceVisibility: string;
-  variants: [
-    {
-      id: string;
-      available: boolean;
-      cost: number;
-      currency: string;
-      depth: number;
-      height: number;
-      width: number;
-      manufacturerItemCode: string;
-      manufacturerItemId: string;
-      description: string;
-      packaging: string;
-      volume: number;
-      volumeUom: string;
-      weight: number;
-      weightUom: string;
-      optionName: string;
-      optionsPath: string;
-      optionItemsPath: string;
-      sku: string;
-      active: boolean;
-      images: [
-        {
-          fileName: string;
-          cdnLink: string;
-          i: number;
-          alt: string;
-        },
-      ];
-      itemCode: string;
-    },
-  ];
-  options: [
-    {
-      id: string;
-      name: string;
-      dataField: string;
-      values: [
-        {
-          id: string;
-          name: string;
-          value: string;
-        },
-      ];
-    },
-  ];
+
+  @ApiProperty({ isArray: true })
+  variants: Variant[];
+
+  @ApiProperty({ isArray: true })
+  options: Option[];
   availability: string;
   isFragile: boolean;
   published: string;
@@ -77,3 +43,48 @@ export enum ProductOptions {
   packaging = 'packaging',
   description = 'description',
 }
+
+type Variant = {
+  id: string;
+  available: boolean;
+  cost: number;
+  currency: string;
+  depth: number;
+  height: number;
+  width: number;
+  manufacturerItemCode: string;
+  manufacturerItemId: string;
+  description: string;
+  packaging: string;
+  volume: number;
+  volumeUom: string;
+  weight: number;
+  weightUom: string;
+  optionName: string;
+  optionsPath: string;
+  optionItemsPath: string;
+  sku: string;
+  active: boolean;
+  images: Image[];
+  itemCode: string;
+};
+
+type Image = {
+  fileName: string;
+  cdnLink: string;
+  i: number;
+  alt: string;
+};
+
+type Option = {
+  id: string;
+  name: string;
+  dataField: string;
+  values: [
+    {
+      id: string;
+      name: string;
+      value: string;
+    },
+  ];
+};
